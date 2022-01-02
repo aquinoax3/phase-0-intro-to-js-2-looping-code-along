15b22526bd9c5d4224ede70d9c2358066b65efa9
Axel Aquino <aquino.axel@gmail.com>
2022-01-02 18:12:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+
+
+
+
+
+
+
+function writeCards(cardName1, event) {
+    let messages = []
+    for (let i = 0; i < cardName1.length; i++) {
+        messages.push(`Thank you, ${cardName1[i]}, for the wonderful surprise gift!`);
+         
+    }
+    return messages;
+}
+
+console.log(writeCards("bill"))
+
+
+function countDown(positiveNumber) {
+      while (positiveNumber >= 0) {
+         console.log(positiveNumber--)   
+      }
+      return positiveNumber;
+          
+  }
+
+  console.log(countDown(10));
